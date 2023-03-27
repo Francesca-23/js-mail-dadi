8c09637717ffae526afd0129f98fe3404c76660e
Francesca-23 <francescadidomenico4@gmail.com>
2023-03-27 16:54:11 +0200

    Creazione struttura per l'input mail

---
+++ Mail/js/main.js
@@ -0,0 +1,51 @@
+/*
+1-Chiedi all’utente la sua email.
+    -input
+2-controlla che sia nella lista di chi può accedere.
+    -array di email
+    -ciclo per l'array
+    -condizione if
+3-stampa un messaggio appropriato sull’esito del controllo.
+    -document.querySelector()
+*/
+
+
+
+
+function controlloMail(){
+    
+    //array emails
+    let emails = [
+        "pincopallo@gmail.com",
+        "laurarossi@gmail.com",
+        "costanzolibero@gmail.com",
+        "anitacolombo@libero.it"
+    ];
+
+    //inserimento mail 
+    let email = document.getElementById('emailCliente').value;
+
+    let createP = document.querySelector('#container');
+    let p = document.createElement('p');
+    p.className = "text-center bg-info-subtle rounded p-2";
+
+
+    for(let i=0; i <= emails.length; i++){
+
+        if(email == emails[i]){
+
+            p.innerHTML = "L'email è corretta";
+            createP.append(p);
+
+        }else{
+
+            p.innerHTML = "L'email non esiste";
+            createP.append(p);
+            
+        }
+    }
+
+}
+
+
+
